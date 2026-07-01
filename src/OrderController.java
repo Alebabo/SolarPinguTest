a485a795f7e9a933550ecad68494b84830f4fce3
Alexander Saade <alexander.saade@tum.de>
2026-07-01 10:54:49 +0200

    habibi

--- src/OrderController.java
+++ src/OrderController.java
@@ -3,13 +3,15 @@ import java.util.ArrayList;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @RestController
 public class OrderController {
     private WebClient webClient;
     private ArrayList<String> orders;
+    private Ordercordinator ordercordinator;
     public OrderController() {
         this.webClient = WebClient.builder()
                 .baseUrl("http://localhost:8080")
@@ -17,5 +19,36 @@ public class OrderController {
                 .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
                 .build();
         this.orders = new ArrayList<>();
+        ordercordinator = new Ordercordinator();
     }
+    @GetMapping("orders")
+    public ResponseEntity<ArrayList<Order>> getAllOrders(@RequestParam(value = "from", defaultValue = "") Long from,
+                                                    @RequestParam(value = "to", defaultValue = "") Long to) {
+        return ResponseEntity.ok(orderService.getAllOrders(to, from));
+    }
+    public void getAllOrders(Long from, Long to, Consumer<List<Order>> ordersConsumer) {
+        webClient.get()
+                .uri(uriBuilder -> uriBuilder
+                        .path("orders")
+                        .queryParam("from", from)
+                        .queryParam("to", to)
+                        .build())
+                .retrieve()
+                .bodyToMono(new ParameterizedTypeReference<List<Order>>() {})
+                .onErrorStop()
+                .subscribe(loadedOrders -> {
+                    orders.clear();
+                    orders.addAll(loadedOrders);
+                    ordersConsumer.accept(orders);
+                });
+    }
+
+    @PutMapping("customers/{customerId}")
+    public ResponseEntity<Customer> updateCustomer(@RequestBody Customer updatedCustomer, @PathVariable("customerId") UUID personId) {
+        if (!updatedCustomer.getId().equals(personId)) {
+            return ResponseEntity.badRequest().build();
+        }
+        return ResponseEntity.ok(customerService.savePerson(updatedCustomer));
+    }
+
 }
